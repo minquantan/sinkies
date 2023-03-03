f11f467cc0f13973d6fbde0982bdce251b716590
Minquan Tan <minch_tan@hotmail.com>
2023-03-03 21:40:47 +0800

    didn't know that heroku database is now a paid service. gonna ignore hosting for now. able to call on all main courses to be displayed, now styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,11 @@ class PagesController < ApplicationController
   def home; end
 
   def menu
-    @photo_array = []
-    SetMeal.all.each do |food|
-      food.photos.each do |photo|
-        @photo_array << photo
-      end
-    end
+    @main_courses = Food.where(menu_category: 'main course')
+    @snacks = Food.where(menu_category: 'snack')
+    @desserts = Food.where(menu_category: 'dessert')
+    @set_meals = SetMeal.all
+    @drinks = Drink.all
   end
 
   def space; end
