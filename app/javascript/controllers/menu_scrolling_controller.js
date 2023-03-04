b0fe1cacf52bb98a0e06859ad6b9db573e97f283
Minquan Tan <minch_tan@hotmail.com>
2023-03-04 11:05:47 +0800

    weird horizontal scrollbar. refuses to inherit width from parent...

--- app/javascript/controllers/menu_scrolling_controller.js
+++ app/javascript/controllers/menu_scrolling_controller.js
@@ -1,5 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  console.log(rect)
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 // Connects to data-controller="menu-scrolling"
 export default class extends Controller {
   connect() {
@@ -8,5 +20,23 @@ export default class extends Controller {
 
   colors() {
     console.log("im back again")
+    const menuCategories = document.querySelectorAll("#categories h3")
+    const menuDisplays = document.querySelectorAll('#menu-displays > div')
+
+    // menuCategories.forEach((category) => {
+    //   console.log(category.innerHTML)
+    // })
+
+    menuDisplays.forEach((display) => {
+      console.log(typeof(display))
+      var rect = display.getBoundingClientRect();
+      console.log(display.innerHTML)
+      console.log(rect.top, rect.right, rect.bottom, rect.left);
+    })
+
+    // menuCategories.forEach((category) => {
+    //   console.log(isInViewport(category))
+    // })
+
   }
 }
