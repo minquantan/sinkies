bb0fd9c48c5fedd52341070f196a8653cee31306
Minquan Tan <minch_tan@hotmail.com>
2023-03-04 17:53:08 +0800

    active-red for main courses when scrolling

--- app/javascript/controllers/menu_scrolling_controller.js
+++ app/javascript/controllers/menu_scrolling_controller.js
@@ -1,17 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-
 // Connects to data-controller="menu-scrolling"
 export default class extends Controller {
   connect() {
@@ -27,16 +16,20 @@ export default class extends Controller {
     const sets = document.getElementById("sets");
     const drinks = document.getElementById("drinks");
 
-    console.log("im back again");
-
     const mainsRect = mains.getBoundingClientRect();
     const snacksRect = snacks.getBoundingClientRect();
     const dessertsRect = desserts.getBoundingClientRect();
     const setsRect = sets.getBoundingClientRect();
     const drinksRect = drinks.getBoundingClientRect();
 
+    const catMain = document.getElementById("cat-main");
+    const catSnack = document.getElementById("cat-snack");
+    const catDessert = document.getElementById("cat-dessert");
+    const catDrink = document.getElementById("cat-drink");
+    const catSet = document.getElementById("cat-set");
 
-    const clientHeight = document.documentElement.clientHeight;
+
+    // const clientHeight = document.documentElement.clientHeight;
     // const mainsHeight = mains.getBoundingClientRect().y;
     // console.log(clientHeight);
     // console.log(mainsHeight);
@@ -47,22 +40,32 @@ export default class extends Controller {
 
     // if (clientHeight > mainsTop && clientHeight > mainsBottom) {
     //   console.log(clientHeight)
-    //   console.log(mainsRect.top)
-    //   console.log(mainsRect.bottom)
+      console.log(mainsRect.top)
+      // console.log(mainsRect.bottom)
     // }
 
-    // if (
-    //   drinksRect.top >= 0 &&
-    //   drinksRect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-    //   console.log(drinksRect.top)
-    //   console.log(drinksRect.bottom)
-    // }
+    if (251 > mainsRect.top &&
+      mainsRect.top > -909 &&
+      1516 > mainsRect.bottom &&
+      mainsRect.bottom > 356){
+        console.log("the main courses are in the viewport you nigga worm!");
+        // debugger
+        catMain.classList.add("red-active");
+        console.log(catMain.innerText)
 
-    // console.log(drinksRect.top)
-    // console.log(drinksRect.bottom)
 
-    console.log(setsRect.top)
-    console.log(setsRect.bottom)
+    } else {
+      console.log("you refuse to work?!");
+    }
 
+
+
+    // at the top of the main courses
+    // 250.66250610351562 top value
+    // 1515.6625061035156 bottom value
+
+    // at the bottom of the main courses
+    // -908.5375366210938 top value
+    // 356.46246337890625 bottom value
   }
 }
