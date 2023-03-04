3a78bf1ed4bc7830c81987bea21f0fed9155805c
Minquan Tan <minch_tan@hotmail.com>
2023-03-04 13:49:21 +0800

    will hard code the boundaries or consider just using links to access. scrolling is beyond me because the items are too large. they span several vh.

--- app/javascript/controllers/menu_scrolling_controller.js
+++ app/javascript/controllers/menu_scrolling_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  console.log(rect)
   return (
       rect.top >= 0 &&
       rect.left >= 0 &&
@@ -12,31 +11,58 @@ function isInViewport(element) {
   );
 }
 
+
 // Connects to data-controller="menu-scrolling"
 export default class extends Controller {
   connect() {
     console.log("the menu-scrolling controller is up")
   }
 
+
+
   colors() {
-    console.log("im back again")
-    const menuCategories = document.querySelectorAll("#categories h3")
-    const menuDisplays = document.querySelectorAll('#menu-displays > div')
+    const mains = document.getElementById("mains");
+    const snacks = document.getElementById("snacks");
+    const desserts = document.getElementById("desserts");
+    const sets = document.getElementById("sets");
+    const drinks = document.getElementById("drinks");
 
-    // menuCategories.forEach((category) => {
-    //   console.log(category.innerHTML)
-    // })
+    console.log("im back again");
 
-    menuDisplays.forEach((display) => {
-      console.log(typeof(display))
-      var rect = display.getBoundingClientRect();
-      console.log(display.innerHTML)
-      console.log(rect.top, rect.right, rect.bottom, rect.left);
-    })
+    const mainsRect = mains.getBoundingClientRect();
+    const snacksRect = snacks.getBoundingClientRect();
+    const dessertsRect = desserts.getBoundingClientRect();
+    const setsRect = sets.getBoundingClientRect();
+    const drinksRect = drinks.getBoundingClientRect();
 
-    // menuCategories.forEach((category) => {
-    //   console.log(isInViewport(category))
-    // })
+
+    const clientHeight = document.documentElement.clientHeight;
+    // const mainsHeight = mains.getBoundingClientRect().y;
+    // console.log(clientHeight);
+    // console.log(mainsHeight);
+
+    // this means that we are scrolling down, and therefore away from the mains section.
+
+
+
+    // if (clientHeight > mainsTop && clientHeight > mainsBottom) {
+    //   console.log(clientHeight)
+    //   console.log(mainsRect.top)
+    //   console.log(mainsRect.bottom)
+    // }
+
+    // if (
+    //   drinksRect.top >= 0 &&
+    //   drinksRect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+    //   console.log(drinksRect.top)
+    //   console.log(drinksRect.bottom)
+    // }
+
+    // console.log(drinksRect.top)
+    // console.log(drinksRect.bottom)
+
+    console.log(setsRect.top)
+    console.log(setsRect.bottom)
 
   }
 }
