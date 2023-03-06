ead499a41bce696c58d53621b6e7e7710aa6b50e
Minquan Tan <minch_tan@hotmail.com>
2023-03-06 20:46:18 +0800

    used someones code to rectify the issue of dom object being too big to practically check if in viewport

--- app/javascript/controllers/menu_scrolling_controller.js
+++ app/javascript/controllers/menu_scrolling_controller.js
@@ -8,67 +8,124 @@ export default class extends Controller {
   }
 
   colors() {
-    const snacks = document.getElementById("snacks");
-    const snacksRect = snacks.getBoundingClientRect();
+/*     const catMain = document.getElementById("cat-main");
     const catSnack = document.getElementById("cat-snack");
+    const catDessert = document.getElementById("cat-dessert");
+    const catSet = document.getElementById("cat-set");
+    const catDrink = document.getElementById("cat-drink");
+
+    // get all the food categories
+    const mains = document.getElementById("mains");
+    const snacks = document.getElementById("snacks");
+    const desserts = document.getElementById("desserts");
+    const sets = document.getElementById("sets");
+    const drinks = document.getElementById("drinks");
+
+    // get all the individual menu items from each food category
+    const mainTitles = mains.querySelectorAll('.menu-card-title-container');
     const snackTitles = snacks.querySelectorAll('.menu-card-title-container');
+    const dessertTitles = desserts.querySelectorAll('.menu-card-title-container');
+    const setTitles = sets.querySelectorAll('.menu-card-title-container');
+    const drinkTitles = drinks.querySelectorAll('.menu-card-title-container');
+    // console.log(menuTitles)
+    //-------------------------------------------------------------------------------
 
-    const menuCategories = document.querySelectorAll('#categories h3');
-
-    // check if there are any menu categories that are already highlighted as active
-    const activeCategories = [];
-    menuCategories.forEach((category) => {
-      activeCategories.push(category.classList.contains('red-active'));
-    })
-
-    function isTrue(item) {
-      return item === true;
+    // define function to establish if an element is visible in the viewport
+    function isVisible(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
     }
+  //-------------------------------------------------------------------------------
 
-    const areActive = activeCategories.some(isTrue)
+  // check if there are any menu categories that are already highlighted as active
+  const activeCategories = [];
+  const menuCategories = document.querySelectorAll('#categories h3');
+  menuCategories.forEach((category) => {
+    activeCategories.push(category.classList.contains('red'));
+  })
+
+  const areActive = activeCategories.some(isTrue)
+  function isTrue(item) {
+    return item === true;
+  }
+  //-------------------------------------------------------------------------------
+
+  // see if the position of main titles are viewable
+
+  const visibleMains = [];
+  mainTitles.forEach((title) => {
+    console.log(title.innerHTML);
+    visibleMains.push(isVisible(title));
+  })
+  console.log('these are the visible mains', visibleMains);
+  console.log('there are active menu categories', areActive);
+
+  if (visibleMains.includes(true)  && areActive != true ) {
+    catMain.classList.add('red');
+    catMain.insertAdjacentText('test');
+  } else {
+    catMain.classList.remove('red');
+  } */
 
 
+  const isElementXPercentInViewport = function(el, percentVisible) {
+    let
+      rect = el.getBoundingClientRect(),
+      windowHeight = (window.innerHeight || document.documentElement.clientHeight);
 
-    let titleIsVisibleArray = []
-    snackTitles.forEach((title)=>{
-      // get the coordinates of the menu item's name
-      const titleRect = title.getBoundingClientRect();
+    return !(
+      Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentVisible ||
+      Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+    )
+  };
 
-      /* set a variable that gives true or false as to whether the coordinates are within the viewport */
-      var is_visible = titleRect.top >= 0 && titleRect.left >= 0 &&
-      titleRect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      titleRect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  const mains = document.getElementById("mains");
+  const snacks = document.getElementById("snacks");
+  const desserts = document.getElementById("desserts");
+  const sets = document.getElementById("sets");
+  const drinks = document.getElementById("drinks");
 
-      // push the result of whether picture is visible into array
-      titleIsVisibleArray.push(is_visible);
-    })
+  const catMain = document.getElementById("cat-main");
+  const catSnack = document.getElementById("cat-snack");
+  const catDessert = document.getElementById("cat-dessert");
+  const catSet = document.getElementById("cat-set");
+  const catDrink = document.getElementById("cat-drink");
 
-    // check whether there is any true in the array. if yes then change the color. if no then remove the color.
-    console.log(titleIsVisibleArray);
-    if (titleIsVisibleArray.includes(true) && areActive != true ) {
-      catSnack.classList.add('red-active');
-    } else {
-      catSnack.classList.remove('red-active');
-    }
-/* only go red if every other category is not red */
+  if (isElementXPercentInViewport(mains, 30)) {
+    catMain.classList.add('red');
+  } else {
+    catMain.classList.remove('red');
+  }
 
-    console.log(menuCategories);
-    console.log(activeCategories);
-    console.log(areActive);
+  if (isElementXPercentInViewport(snacks, 60)) {
+    catSnack.classList.add('red');
+  } else {
+    catSnack.classList.remove('red');
+  }
+
+  if (isElementXPercentInViewport(desserts, 60)) {
+    catDessert.classList.add('red');
+  } else {
+    catDessert.classList.remove('red');
+  }
+
+  if (isElementXPercentInViewport(sets, 60)) {
+    catSet.classList.add('red');
+  } else {
+    catSet.classList.remove('red');
+  }
+
+  if (isElementXPercentInViewport(drinks, 20)) {
+    catDrink.classList.add('red');
+  } else {
+    catDrink.classList.remove('red');
+  }
+
 
   }
 }
-              // const mains = document.getElementById("mains");
-              // const desserts = document.getElementById("desserts");
-              // const sets = document.getElementById("sets");
-              // const drinks = document.getElementById("drinks");
-
-              // const mainsRect = mains.getBoundingClientRect();
-              // const dessertsRect = desserts.getBoundingClientRect();
-              // const setsRect = sets.getBoundingClientRect();
-              // const drinksRect = drinks.getBoundingClientRect();
-
-              // const catMain = document.getElementById("cat-main");
-              // const catDessert = document.getElementById("cat-dessert");
-              // const catDrink = document.getElementById("cat-drink");
-              // const catSet = document.getElementById("cat-set");
