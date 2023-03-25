99a0e514a696c83e628708f92a1c7c3a28c7d960
Minquan Tan <minch_tan@hotmail.com>
2023-03-26 01:47:53 +0800

    gallery modal is receiving the right variables

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -9,9 +9,27 @@ export default class extends Controller {
   popout() {
     const name = this.element.querySelectorAll("p")[0].innerText;
     const description = this.element.querySelectorAll("p")[1].innerText;
-    const image = this.element.querySelector("img");
+    const image = this.element.querySelector("img").src;
+    console.log(image);
+
 
     const modal = document.getElementById("gallery-modal");
-    console.log(modal);
+    modal.innerHTML = `
+      <img src="${image}">
+      <div id="gallery-modal-details">
+        <div style="display:flex;align-items:center;justify-content:space-between">
+          <h4 style="margin:0">${name}</h4>
+          <div id="back-invitation" class="quit-modal">
+            <div>
+              <p style="margin-top:8px;margin-bottom:0px;font-size:14px">BACK</p>
+            </div>
+            <div id="back-button" style="width:30px;display:flex;align-items:center">
+              <img src="https://res.cloudinary.com/minquant/image/upload/v1679308978/mascott_stretch_dyhqqi.png" alt="">
+            </div>
+          </div>
+        </div>
+        <p>${description}</p>
+      </div>
+    `
   }
 }
