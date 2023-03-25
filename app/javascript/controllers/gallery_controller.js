c951c9534ec7d6458cbffce5b66d96314b266713
Minquan Tan <minch_tan@hotmail.com>
2023-03-25 14:14:47 +0800

    gallery controller js created

---
+++ app/javascript/controllers/gallery_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log("let's try to pop out these gallery images")
+  }
+
+  popout() {
+    console.log("booya nibba");
+  }
+}
