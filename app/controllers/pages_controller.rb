c54e02bc493a71c73c1e40e8ba73bf5b0c0093f5
Minquan Tan <minch_tan@hotmail.com>
2023-04-01 16:21:25 +0800

    hid api key

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,13 +9,7 @@ class PagesController < ApplicationController
 
   def about; end
 
-  def contact
-    @main_courses = Food.where(menu_category: 'main course')
-    @snacks = Food.where(menu_category: 'snack')
-    @desserts = Food.where(menu_category: 'dessert')
-    @set_meals = SetMeal.all
-    @drinks = Drink.all
-  end
+  def contact; end
 
   def reservations; end
 end
